6bdff3d4c97a809f69e59a63f5167e9d570b1504
Matheus Silva <matheus_lsilva@hotmail.com>
2023-11-27 19:33:24 -0300

    chore: initial setup

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\Site\SiteController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/contact', [SiteController::class, 'contact']);
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
